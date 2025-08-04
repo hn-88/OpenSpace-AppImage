aaa9c733e22aee01959c6a0214def3f7bc2b35f3
hn-88 <hn-88@users.noreply.github.com>
2025-08-04 13:42:49 +0530

    Delete runopenspace0.21.2_x86_64.sh

--- runopenspace0.21.2_x86_64.sh
+++
@@ -1,4 +0,0 @@
-export QT_PLUGIN_PATH=lib/x86_64-linux-gnu/qt6/plugins/
-export OPENSPACE_USER="$PWD/OpenSpace/user"  # <-- change this path to some directory where you have write access
-export OPENSPACE_GLOBEBROWSING="$PWD/OpenSpaceData" # <-- change this path to some directory where you have write access
-./OpenSpace-0.21.2*64.AppImage
