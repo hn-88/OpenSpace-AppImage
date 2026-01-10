970cb39170ca0c40d0bbcc68b718443617ed131d
hn-88 <hn-88@users.noreply.github.com>
2026-01-10 10:20:43 +0530

    Update create-patch.sh
    LC_ALL=C is needed for sed also

--- MacOS-patches/create-patch.sh
+++ MacOS-patches/create-patch.sh
@@ -1,2 +1,3 @@
 cd working-dir
-LC_ALL=C diff -ruN /full/path/to/old-dir/ ./ | sed 's|/full/path/to/old-dir/||g; s|./||g' > changes.diff
+export LC_ALL=C
+diff -ruN /full/path/to/old-dir/ ./ | sed 's|/full/path/to/old-dir/||g; s|./||g' > changes.diff
